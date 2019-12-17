69ab5dd8137ca77c3ab8a05043434360a32c40e2
batavinash <git.batavinash@gmail.com>
2019-12-18 02:02:27 +0530

    fixed another typo

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -12,7 +12,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles"
+    "django.contrib.staticfiles",
     'main'
 ]
 
