11a0828752687b09fb49f0986efd44468206b9f2
batavinash <git.batavinash@gmail.com>
2019-12-18 02:42:07 +0530

    removed unwanted code

--- src/boiler/urls.py
+++ src/boiler/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
-from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', home, name='home')
 ]
 
 
